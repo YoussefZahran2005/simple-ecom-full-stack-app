d598ec275a23a1471ba2bfb5bbde92c3e07ca9a2
YoussefZahran2005 <youssef.assem.zahran@gmail.com>
2026-09-19 10:56:06 +0300

    intial commit

---
+++ backend/src/main/java/com/ecom/zahranStore/ZahranStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.zahranStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZahranStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZahranStoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecom/zahranStore/ZahranStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.zahranStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZahranStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
